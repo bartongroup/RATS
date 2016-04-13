8511f8be5f61ea7ef0288edc1a2d00397548d77f
kiramt <k.mourao@dundee.ac.uk>
2016-04-13 12:03:49 +0100

    Added dplyr library

--- R/rats.R
+++ R/rats.R
@@ -3,6 +3,9 @@
 #' @param df a data frame with at least two variables, \code{target_id} & \code{parent_id}
 #'
 #' @export
+
+library(dplyr)
+
 markSingles <- function(df){
   parent_group <- group_by(df, parent_id)
   unique_parent_count<-summarise(parent_group,n())
