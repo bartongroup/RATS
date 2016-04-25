d3a566f6015cfb0361f9cdddb555c07e89866f26
kiramt <k.mourao@dundee.ac.uk>
2016-04-25 13:51:52 +0100

    Generalised filtering in calculate_tx_proportions so that each bootstrap has its own filter.

--- tests/testthat/test_rats.R
+++ tests/testthat/test_rats.R
@@ -8,15 +8,15 @@ test_that("Stats are correct", {
   stats <- calculate_tx_proportions(pseudo_sleuth, mini_anno)
 
   m <- c(458.99861,41.24901,55.08233,12.53521)
-  expect_equal(stats$Col$mean, m, tolerance = 1e-6)
+  expect_equal(sort(stats$Col$mean), sort(m), tolerance = 1e-6)
 
   v <- c(476.89568,144.82521,43.95496,40.20190)
-  expect_equal(stats$Col$variance, v, tolerance = 1e-6)
+  expect_equal(sort(stats$Col$variance), sort(v), tolerance = 1e-6)
 
   m <- c(411.072110,34.354291,46.807238,6.068015)
-  expect_equal(stats$Vir$mean, m, tolerance = 1e-6)
+  expect_equal(sort(stats$Vir$mean), sort(m), tolerance = 1e-6)
 
   v <- c(710.51429,62.14951,56.38875,11.68474)
-  expect_equal(stats$Vir$variance, v, tolerance = 1e-6)
+  expect_equal(sort(stats$Vir$variance), sort(v), tolerance = 1e-6)
 
 })
