7d1c387397aa503942c841a0a458ba4bf5ec58e2
fruce-ki <jack_ohara_097@hotmail.com>
2016-05-03 17:10:59 +0100

    Bugfixes in tests. Completed structure and munging test. Error in testing for 0 values.

--- tests/testthat/test_rats.R
+++ tests/testthat/test_rats.R
@@ -64,95 +64,83 @@ test_that("The data munging is correct", {
   expect_identical(ordered(levels(as.factor(ids$target_id))), ordered(levels(as.factor(tx_filter$target_id))))
   # count parent duplicates and make sure flag was assigned correctly
   parent_counts <- table(ids$parent_id)
-  for (parent in parent_counts) {
+  for (parent in names(parent_counts)) {
     expect_true(all((parent_counts[[parent]] > 1) == (tx_filter$has_siblings[tx_filter$parent_id == parent])))  # lengths may differ
   }
 
-#   samples_by_condition <- group_samples(sl$sample_to_covariates)
-#   # all the variables are here:
-#   expect_identical(names(sl$sample_to_covariates), names(samples_by_condition))
-#   # all the variable values are here:
-#   for (cond in sl$sample_to_covariates){
-#     expect_true(any( ordered(names(samples_by_condition[[cond]])) == ordered(levels(as.factor(sl$sample_to_covariates[[cond]]))) ))
-#     # the correct number of samples have been assigned:
-#     cond_counts <- table(sl$sample_to_covariates[[cond]])
-#     for (subcond in samples_by_condition[[cond]]) {
-#       expect_equal(cond_counts[sub_cont], length(samples_by_condition[[cond]][[sub_cond]]))
-#       # the samples assigned are the correct ones:
-#       for (sample in samples_by_condition[[cond]][[sub_cond]]) {
-#         expect_equal(sl$sample_to_covariates[sample, cond], sub_cond)
-#       }
-#     }
-#   }
-#   samples_by_condition <- samples_by_condition[[varname]]  # focus on one covariate for the rest of the test.
-#
-#   count_data <- lapply(samples_by_condition, function(condition) make_filtered_bootstraps(sl, condition, tx_filter, counts_col))
-#   for (condition in count_data) {
-#     # all the targets are here:
-#     expect_identical(ordered(rownames(condition)), ordered(tx_filter$target_id[tx_filter$has_siblings]))
-#     # the correct number of counts have been gathered:
-#     tot_boots <- 0
-#     # the correct counts have been assigned:
-#     for (sample in samples_by_condition[[condition]]) {
-#       for (boot in sl$kal[[sample]]) {
-#         for (target in rownames(condition)) {
-#           expect_true(boot[boot$target_id == target, counts_col] %in% condition[target, ])
-#         }
-#         tot_boots <- tot_boots + 1
-#       }
-#     }
-#     expect_equal(tot_boots, dim(condition)[2])
-#   }
+  samples_by_condition <- group_samples(sl$sample_to_covariates)
+  # all the variables are here:
+  expect_identical(names(sl$sample_to_covariates), names(samples_by_condition))
+  # all the variable values are here:
+  for (condname in names(sl$sample_to_covariates)){
+    expect_true(any( ordered(names(samples_by_condition[[condname]])) == ordered(levels(as.factor(sl$sample_to_covariates[[condname]]))) ))
+    # the correct number of samples have been assigned:
+    cond_counts <- table(sl$sample_to_covariates[[condname]])
+    for (sub_cond in names(samples_by_condition[[condname]])) {
+      expect_equal(cond_counts[[sub_cond]], length(samples_by_condition[[condname]][[sub_cond]]))
+      # the samples assigned are the correct ones:
+      for (smpl in samples_by_condition[[condname]][[sub_cond]]) {
+        expect_equal(sl$sample_to_covariates[smpl, condname], sub_cond)
+      }
+    }
+  }
+  samples_by_condition <- samples_by_condition[[varname]]  # focus on one covariate for the rest of the test.
+
+  count_data <- lapply(samples_by_condition, function(condition) make_filtered_bootstraps(sl, condition, tx_filter, counts_col))
+  for (cond_name in names(count_data)) {
+    # all the targets are here:
+    expect_identical(ordered(rownames(count_data[[cond_name]])), ordered(tx_filter$target_id[tx_filter$has_siblings]))
+    # the correct number of counts have been gathered:
+    tot_boots <- 0
+    # the correct counts have been assigned:
+    for (smpl in samples_by_condition[[cond_name]]) {
+      for (boot in sl$kal[[smpl]]$bootstrap) {
+        for (target in rownames(count_data[[cond_name]])) {
+          expect_true(boot[boot$target_id == target, counts_col] %in% count_data[[cond_name]][target, ])
+        }
+        tot_boots <- tot_boots + 1
+      }
+    }
+    expect_equal(tot_boots, dim(count_data[[cond_name]])[2])
+  }
 })
 
-# #==============================================================================
-# check_equal <- function(result1, result2) {
-#
-#   expect_equal(sort(result1$Col$mean), sort(result2$Col$mean), tolerance = 1e-6)
-#   expect_equal(sort(result1$Col$variance), sort(result2$Col$variance), tolerance = 1e-6)
-#   expect_equal(sort(result1$Col$proportion), sort(result2$Col$proportion), tolerance = 1e-6)
-#   expect_equal(sort(result1$Vir$mean), sort(result2$Vir$mean), tolerance = 1e-6)
-#   expect_equal(sort(result1$Vir$variance), sort(result2$Vir$variance), tolerance = 1e-6)
-#   expect_equal(sort(result1$Vir$proportion), sort(result2$Vir$proportion), tolerance = 1e-6)
-# }
-#
-# test_that("Mixed order bootstraps give same results as unmixed", {
-#
-#   # make a pseudo sleuth object with mixed up bootstrap rows
-#   mixed_pseudo_sleuth <- pseudo_sleuth
-#   mixed_pseudo_sleuth$kal[[1]]$bootstrap[[3]] <- mixed_pseudo_sleuth$kal[[1]]$bootstrap[[3]][c(1,3,2,5,4),]
-#   mixed_pseudo_sleuth$kal[[1]]$bootstrap[[4]] <- mixed_pseudo_sleuth$kal[[1]]$bootstrap[[4]][c(5,4,3,2,1),]
-#   mixed_pseudo_sleuth$kal[[3]]$bootstrap[[1]] <- mixed_pseudo_sleuth$kal[[3]]$bootstrap[[1]][c(3,5,1,4,2),]
-#
-#   mixed_stats <- calculate_DTU(mixed_pseudo_sleuth, mini_anno)
-#   unmixed_stats <- calculate_DTU(pseudo_sleuth, mini_anno)
-#
-#   check_equal(mixed_stats, unmixed_stats)
-# })
-#
-# #==============================================================================
-# test_that("Bootstraps with all 0 / NA entries are discarded", {
-#
-#   # add a new transcript which will have zero entries
-#   TRANSCRIPT <- "AT1G01020.3"
-#   GENE <- "AT1G01020"
-#   z_mini_anno <- rbind(mini_anno, c(TRANSCRIPT, GENE,""))
-#
-#   # make a new sleuth with this transcript added, and give it 0 entries
-#   zero_entry_sleuth <- pseudo_sleuth
-#   zero_entry_sleuth$kal <- lapply(zero_entry_sleuth$kal, function(kal)
-#     lapply(kal, function(bs) lapply(bs, rbind, list(TRANSCRIPT, 0, 0))))
-#
-#   # make a new sleuth with one of the zero entries set to NA
-#   na_entry_sleuth <- zero_entry_sleuth
-#   na_entry_sleuth$kal[[2]]$bootstrap[[1]][6,2] <- NA
-#
-#   # calculate DTU for new sleuths and old one
-#   z_result <- calculate_DTU(zero_entry_sleuth, z_mini_anno)
-#   n_result <- calculate_DTU(na_entry_sleuth, z_mini_anno)
-#   result <- calculate_DTU(pseudo_sleuth, mini_anno)
-#
-#   # check results are all equal
-#   check_equal(z_result, result)
-#   check_equal(n_result, result)
-# })
+#==============================================================================
+test_that("Mixed order bootstraps give same results as unmixed", {
+
+  # make a pseudo sleuth object with mixed up bootstrap rows
+  mixed_pseudo_sleuth <- pseudo_sleuth
+  mixed_pseudo_sleuth$kal[[1]]$bootstrap[[3]] <- mixed_pseudo_sleuth$kal[[1]]$bootstrap[[3]][c(1,3,2,5,4),]
+  mixed_pseudo_sleuth$kal[[1]]$bootstrap[[4]] <- mixed_pseudo_sleuth$kal[[1]]$bootstrap[[4]][c(5,4,3,2,1),]
+  mixed_pseudo_sleuth$kal[[3]]$bootstrap[[1]] <- mixed_pseudo_sleuth$kal[[3]]$bootstrap[[1]][c(3,5,1,4,2),]
+
+  expect_equal(calculate_DTU(mixed_pseudo_sleuth, mini_anno, "Col", "Vir"),
+               calculate_DTU(pseudo_sleuth, mini_anno, "Col", "Vir"))
+})
+
+#==============================================================================
+test_that("Bootstraps with all 0 / NA entries are discarded", {
+
+  # add a new transcript which will have zero entries
+  TRANSCRIPT <- "AT1G01020.3"
+  GENE <- "AT1G01020"
+  z_mini_anno <- rbind(mini_anno, c(TRANSCRIPT, GENE,""))
+
+  # make a new sleuth with this transcript added, and give it 0 entries
+  zero_entry_sleuth <- pseudo_sleuth
+  zero_entry_sleuth$kal <- lapply(zero_entry_sleuth$kal, function(kal)
+    lapply(kal, function(bs) lapply(bs, rbind, list(TRANSCRIPT, 0, 0))))
+
+  # make a new sleuth with one of the zero entries set to NA
+  na_entry_sleuth <- zero_entry_sleuth
+  na_entry_sleuth$kal[[2]]$bootstrap[[1]][6,2] <- NA
+
+  # calculate DTU for new sleuths and old one
+  z_result <- calculate_DTU(zero_entry_sleuth, z_mini_anno, "Col", "Vir")
+  n_result <- calculate_DTU(na_entry_sleuth, z_mini_anno, "Col", "Vir")
+  result <- calculate_DTU(pseudo_sleuth, mini_anno, "Col", "Vir")
+
+  # check results are all equal
+  expect_equal(z_result, result)
+  expect_equal(n_result, result)
+})
