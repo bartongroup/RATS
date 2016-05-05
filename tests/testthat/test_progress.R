28cc554de5b3e91c57c80d5d78c39f1dd5add8b4
kiramt <k.mourao@dundee.ac.uk>
2016-05-05 09:59:48 +0100

    Added tests for progress update class.

---
+++ tests/testthat/test_progress.R
@@ -0,0 +1,41 @@
+context("Progress updates")
+
+#==============================================================================
+test_that("The text output is correct", {
+
+  # Set up progress updates
+  progress_steps <- data.frame(c(10,20,30),
+                               c("Update1","Update2","Update3"),
+                               stringsAsFactors = FALSE)
+  progress <- TxtProgressUpdate(steps=progress_steps, on=TRUE)
+
+  expect_output(progress <- update(progress), "Update1.....10%")
+  expect_output(progress <- update(progress), "Update2.....20%")
+  expect_output(progress <- update(progress), "Update3.....30%")
+})
+
+#==============================================================================
+test_that("Setting 'on' to false silences text output", {
+
+  # Set up progress bar
+  progress_steps <- data.frame(c(10,20,30),
+                               c("Update1","Update2","Update3"),
+                               stringsAsFactors = FALSE)
+  progress <- TxtProgressUpdate(steps=progress_steps, on=FALSE)
+
+  expect_silent(progress <- update(progress))
+
+})
+
+#==============================================================================
+test_that("Setting 'on' to false silences bar output", {
+
+  # Set up progress bar
+  progress_steps <- data.frame(c(10,20,30),
+                               c("Update1","Update2","Update3"),
+                               stringsAsFactors = FALSE)
+  progress <- BarProgressUpdate(steps=progress_steps, on=FALSE)
+
+  expect_silent(progress <- update(progress))
+
+})
