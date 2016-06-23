5cc954156268b80a75577f2c2a0b054ef8990d61
fruce-ki <jack_ohara_097@hotmail.com>
2016-06-23 18:51:54 +0100

    Hugely faster prop.test fully functional. Obscure bug in transfering dtu between tables.

--- R/rats.R
+++ R/rats.R
@@ -1,15 +1,15 @@
 #================================================================================
 #' Calculate differential transcript usage.
 #'
-#' @param sleuth_data A sleuth object.
+#' @param slo A sleuth object.
 #' @param annot A dataframe matching the transcript identifiers to their corresponding gene identifiers.
 #' @param name_A The name for one condition, as it appears in the \code{sample_to_covariates} table within the sleuth object.
 #' @param name_B The name for the other condition, as it appears in the \code{sample_to_covariates} table within the sleuth object.
 #' @param varname The name of the covariate to which the two conditions belong, as it appears in the \code{sample_to_covariates} table within the sleuth object. Default \code{"condition"}.
 #' @param verbose Display progress updates, default \code{FALSE}.
 #' @param p_thresh The p-value threshold, default 0.05.
-#' @param count_thresh Transcripts with fewer reads will be ignored (default 5).
-#' @param testmode One of "G-test", "proportion-test", "both" (default both).
+#' @param count_thresh Transcripts with fewer reads per replicate than this will be ignored (default 5).
+#' @param testmode One of "G-test", "prop-test", "both" (default both).
 #' @param correction The p-value correction to apply, as defined in \code{stats::p.adjust.methods}, default \code{"BH"}.
 #' @param boots Bootstrap the p-values. Default TRUE.
 #' @param threads Enable parallel processing. Default uses parallel::detectCores(). Try setting to 1 if you are having issues.
@@ -20,9 +20,8 @@
 #' @return List of data tables, with gene-level and transcript-level information.
 #'
 #' @import data.table
-#' @import parallel
 #' @export
-calculate_DTU <- function(sleuth_data, annot, name_A, name_B, varname="condition", 
+calculate_DTU <- function(slo, annot, name_A, name_B, varname="condition", 
                           p_thresh=0.05, count_thresh=5, testmode="both", correction="BH", 
                           verbose=FALSE, boots = TRUE, threads=parallel::detectCores(),
                           COUNTS_COL="est_counts", TARGET_COL="target_id", PARENT_COL="parent_id", BS_TARGET_COL="target_id") {
@@ -34,7 +33,7 @@ calculate_DTU <- function(sleuth_data, annot, name_A, name_B, varname="condition
   progress <- update_progress(progress)
   # Input checks.
   threads <- as.integer(threads)  # Plain numbers default to double, unless integer R syntax is explicitly used.
-  paramcheck <- parameters_good(sleuth_data, annot, name_A, name_B, varname, COUNTS_COL,
+  paramcheck <- parameters_good(slo, annot, name_A, name_B, varname, COUNTS_COL,
                                 correction, p_thresh, TARGET_COL, PARENT_COL, BS_TARGET_COL, verbose, threads, count_thresh, testmode, boots)
   if (paramcheck$error) stop(paramcheck$message)
   
@@ -49,9 +48,7 @@ calculate_DTU <- function(sleuth_data, annot, name_A, name_B, varname="condition
   setkey(tx_filter, target_id)
   
   # Reverse look-up from replicates to covariates.
-  samples_by_condition <- group_samples(sleuth_data$sample_to_covariates)[[varname]]
-
-
+  samples_by_condition <- group_samples(slo$sample_to_covariates)[[varname]]
   
   #---------- EXTRACT DATA
   
@@ -60,8 +57,8 @@ calculate_DTU <- function(sleuth_data, annot, name_A, name_B, varname="condition
   # For each condition, a list holds a dataframe for each replicate. 
   # Each dataframe holds the counts from ALL the bootstraps AND the mean counts across the bootstraps, indexed by transcript.
   # Transcripts found in the bootstraps but missing from the annotation are left out.
-  data_A <- denest_boots(sleuth_data, tx_filter$target_id, samples_by_condition[[name_A]], COUNTS_COL, BS_TARGET_COL )
-  data_B <- denest_boots(sleuth_data, tx_filter$target_id, samples_by_condition[[name_B]], COUNTS_COL, BS_TARGET_COL )
+  data_A <- denest_boots(slo, tx_filter$target_id, samples_by_condition[[name_A]], COUNTS_COL, BS_TARGET_COL )
+  data_B <- denest_boots(slo, tx_filter$target_id, samples_by_condition[[name_B]], COUNTS_COL, BS_TARGET_COL )
   bootmeans_A <- as.data.table(lapply(data_A, function(b) b[, mean_count]))
   bootmeans_B <- as.data.table(lapply(data_B, function(b) b[, mean_count]))
   
@@ -97,22 +94,18 @@ calculate_DTU <- function(sleuth_data, annot, name_A, name_B, varname="condition
   resobj$Transcripts[, propB := sumB/totalB]
   resobj$Transcripts[, Dprop := propB - propA]
   
-  # Filter transcripts to reduce number of tests:
-  # Exclude transcripts with no detected sibling isoforms (proportion == 1 in both conditions)
-  # Exclude transcripts that where not detected (proportion == 0 in both conditions).
-  resobj$Transcripts[, test_elig := (propA + propB != 0  &  propA + propB != 2)]
+  # Filter transcripts and genes to reduce number of tests:
+  resobj$Transcripts[, test_elig := (propA + propB > 0  &  
+                                     propA + propB < 2  &  
+                                     sumA > resobj$Parameters[["num_replic_A"]] * count_thresh  &  
+                                     sumB > resobj$Parameters[["num_replic_B"]] * count_thresh)]
+  resobj$Genes[, usable_transc := resobj$Transcripts[, .(parent_id, ifelse(test_elig, 1, 0))][, sum(V2), by = parent_id][, V1] ]  # Assign 1 for eligible transcripts, 0 for non, then sum to get the number of eligible trascripts per gene.
+  resobj$Gene[, test_elig := usable_transc >= 2]
+
+  #---------- TESTS
     
-  #---------- PREP
-  
   progress <- update_progress(progress)
-  # Initialize workers cluster.
-  wcl <- makeCluster(threads, type="PSOCK")  # PSOCK is the most portable option but may get blocked by over-zealous security software.
-  
-  result <- tryCatch({  # The cluster of workers is persistent, so I need to explicitly dismiss it even if execution is interrupted by errors.
-    
-    #---------- TESTS
-    
-    #   progress <- update_progress(progress)
+  if (testmode %in% c("prop-test", "both")) {
     #   # G test, only for parents and targets that survived filtering.
     #   # Compare B counts to A ratios:
     #   results$Genes[actual_parents, Gt_pvalAB := parallel::parSapply(wcl, actual_targets_by_parent, function(targets)
@@ -129,8 +122,10 @@ calculate_DTU <- function(sleuth_data, annot, name_A, name_B, varname="condition
     #   results$Genes[, Gt_dtuBA := Gt_pvalBA_corr < p_thresh]
     #   # Find the agreements.
     #   results$Genes[, Gt_DTU := Gt_dtuAB & Gt_dtuBA ]
-    
-    progress <- update_progress(progress)
+  }
+
+  progress <- update_progress(progress)
+  if (testmode %in% c("prop-test", "both")) {
     # Proportion test.
     # The test function is not vectorised, nor easily vectorisable. Looping is needed.
     # Data tables allow values to be changed in place, and the table is pre-allocated. 
@@ -139,36 +134,17 @@ calculate_DTU <- function(sleuth_data, annot, name_A, name_B, varname="condition
         FUN = function(row) { prop.test(x = row[c("sumA", "sumB")], n = row[c("totalA", "totalB")], correct = TRUE)[["p.value"]] } )) ]
     resobj$Transcripts[(test_elig), Pt_pval_corr := p.adjust(Pt_pval, method=correction)]
     resobj$Transcripts[, Pt_DTU := Pt_pval_corr < p_thresh]
+    tmp <- resobj$Transcripts[, any(Pt_DTU), by = parent_id][, V1]
     
-    # Return value for the try block.
-    resobj
+    #resobj$Genes[, Pt_DTU := tmp]
     
-    # Commented out to prevent the execution from aborting on warnings.
-#   }, warning = function(war) {
-#     print(war)
-#     traceback()
-#     if (exists("resobj")) {
-#       resobj  # Inspecting half-baked tables could be useful in understanding some errors.
-#     } else {
-#       NULL
-#     }
     
-  }, error = function(err) {
-    print(err)
-    traceback()
-    if (exists("resobj")) {
-      resobj  # Inspecting half-baked tables could be useful in understanding some errors.
-    } else {
-      NULL
-    }
-    
-  }, finally = {
-    # Dismiss workers.
-    parallel::stopCluster(wcl)
-  })
+  }
+  
+  #---------- DONE
   
   progress <- update_progress(progress)
-  return(result)
+  return(resobj)
 }
 
 
@@ -201,7 +177,6 @@ group_samples <- function(covariates) {
 }
 
 
-
 #================================================================================
 #' Extract bootstrap counts into a less nested structure.
 #' 
@@ -227,8 +202,8 @@ denest_boots <- function(slo, tx, samples, COUNTS_COL, BS_TARGET_COL) {
     setnames(dt, nm, as.character(seq(1, length(nm), 1)))
     # Replace any NAs with 0. Happens when annotation different from that used for DTE.
     dt[is.na(dt)] <- 0
-    # Add mean count per transcript and transcript ID.
-    means <- rowMeans(dt)  # Compute it before I add stuff to the table.
+    # Add mean count and transcript ID.
+    means <- rowMeans(dt)  # Compute it while the table is purely counts.
     dt[, c("mean_count", "target_id") := list(means, tx)]
   })
 }
@@ -247,7 +222,7 @@ alloc_out <- function(annot){
   Genes <- data.table("parent_id"=levels(as.factor(annot$parent_id)),
                       "known_transc"=NA_integer_, "usable_transc"=NA_integer_,
                       "test_elig"=NA,                              # eligible for testing (reduce number of tests)
-                      "Gt_DTU"=NA, "Pt_DTU"=NA,
+                      "Pt_DTU"=NA, "Gt_DTU"=NA,
                       "Gt_pvalAB"=NA_real_, "Gt_pvalBA"=NA_real_,
                       "Gt_pvalAB_stdev"=NA_real_, "Gt_pvalBA_stdev"=NA_real_,
                       "Gt_pvalAB_corr"=NA_real_, "Gt_pvalBA_corr"=NA_real_,
@@ -262,7 +237,7 @@ alloc_out <- function(annot){
                             "stdevA"=NA_real_, "stdevB"=NA_real_,  # standard deviation across replicates of means across bootstraps
                             "totalA"=NA_real_, "totalB"=NA_real_)  # sum of all transcripts for that gene
   setkey(Genes, parent_id)
-  setkey(Transcripts, target_id)
+  setkey(Transcripts, parent_id, target_id)
   return(structure(list("Parameters"=Parameters, "Genes"=Genes, "Transcripts"=Transcripts), class="dtu"))
 }
 
@@ -272,24 +247,24 @@ alloc_out <- function(annot){
 #'
 #' @return List with a logical value and a message.
 #'
-parameters_good <- function(sleuth_data, transcripts, ref_name, comp_name, varname, COUNTS_COL,
+parameters_good <- function(slo, annot, ref_name, comp_name, varname, COUNTS_COL,
                             correction, p_thresh, TARGET_COL, PARENT_COL, BS_TARGET_COL, verbose, 
                             threads, count_thresh, testmode, boots) {
-  if ( ! is.data.frame(transcripts))
+  if ( ! is.data.frame(annot))
     return(list("error"=TRUE, "message"="transcripts is not a data.frame!"))
-  if (any( ! c(TARGET_COL, PARENT_COL) %in% names(transcripts)))
+  if (any( ! c(TARGET_COL, PARENT_COL) %in% names(annot)))
     return(list("error"=TRUE, "message"="The specified target and parent IDs field-names do not exist in transcripts!"))
-  if ( ! BS_TARGET_COL %in% names(sleuth_data$kal[[1]]$bootstrap[[1]]))
+  if ( ! BS_TARGET_COL %in% names(slo$kal[[1]]$bootstrap[[1]]))
     return(list("error"=TRUE, "message"="The specified target IDs field-name does not exist in the bootstraps!"))
-  if ( ! COUNTS_COL %in% names(sleuth_data$kal[[1]]$bootstrap[[1]]))
+  if ( ! COUNTS_COL %in% names(slo$kal[[1]]$bootstrap[[1]]))
     return(list("error"=TRUE, "message"="The specified counts field-name does not exist!"))
   if ( ! correction %in% p.adjust.methods)
     return(list("error"=TRUE, "message"="Invalid p-value correction method name. Refer to stats::p.adjust.methods!"))
   if ( ( ! is.numeric(p_thresh)) || p_thresh > 1 || p_thresh < 0)
     return(list("error"=TRUE, "message"="Invalid p-value threshold!"))
-  if ( ! varname %in% names(sleuth_data$sample_to_covariates))
+  if ( ! varname %in% names(slo$sample_to_covariates))
     return(list("error"=TRUE, "message"="The specified covariate name does not exist!"))
-  if ( any( ! c(ref_name, comp_name) %in% sleuth_data$sample_to_covariates[[varname]] ))
+  if ( any( ! c(ref_name, comp_name) %in% slo$sample_to_covariates[[varname]] ))
     return(list("error"=TRUE, "message"="One or both of the specified conditions do not exist!"))
   if ( ! is.logical(verbose))
     return(list("error"=TRUE, "message"="verbose must be a logical value!"))
@@ -315,9 +290,8 @@ parameters_good <- function(sleuth_data, transcripts, ref_name, comp_name, varna
 #'
 init_progress <- function(on)
 {
-  progress_steps <- data.frame(c(1, 3, 7, 10, 20, 25, 35, 70, 100),
+  progress_steps <- data.frame(c(1, 3, 7, 14, 15, 20, 60, 100),
                                c("Checking parameters...",
-                                 "Initializing threads...",
                                  "Creating look-up structures...",
                                  "Extracting counts from bootstraps...",
                                  "Allocating output structure...",
