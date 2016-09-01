9aeed067782399e0acdd5865c5fb79177d345b50
fruce-ki <jack_ohara_097@hotmail.com>
2016-09-01 15:10:54 +0100

    Created GTF id extractor
    To create the needed annotation table.

---
+++ R/annot2ids.R
@@ -0,0 +1,24 @@
+#================================================================================
+#' Extract matching transcript and gene IDs from a GTF file.
+#' 
+#' This function performs no file format validation. Please use the appropriate type.
+#' It is NOT compatible GFF3, as there is no rigid specification for ID attributes.
+#' 
+#' @param annotfile A GTF file.
+#' @param transc_header The tile for the transcripts column in the output. (target_id)
+#' @param gene_header The tile for the genes column in the output. (parent_id)
+#' @return A data.frame with two columns, matching transcript IDs to gene IDs.
+#' 
+#' @export
+#'
+annot2ids <- function(annotfile, transc_header = "target_id", gene_header = "parent_id")
+{
+  annotfile <- "~/Araport11.gff3"
+  annot <- read.delim(annotfile, comment.char = "#", header = FALSE)
+  t2g <- data.frame("transc_id" = gsub(".*transcript_id \"?(.+?)\"?;.*", "\\1", annot[[9]]),
+                    "gene_id" = gsub(".*gene_id \"?(.+?)\"?;.*", "\\1", annot[[9]]) )
+  t2g <- unique(t2g)
+  names(t2g) <- c(transc_header, gene_header)
+  
+  return(t2g)
+}
