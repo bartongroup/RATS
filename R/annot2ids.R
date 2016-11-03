eebe849848247f4ea3d26b68aab3f671bb6767d9
fruce-ki <jack_ohara_097@hotmail.com>
2016-11-03 18:24:52 +0000

    resolves #10

--- R/annot2ids.R
+++ R/annot2ids.R
@@ -7,15 +7,18 @@
 #' @param annotfile A GTF file.
 #' @param transc_header The tile for the transcripts column in the output. (target_id)
 #' @param gene_header The tile for the genes column in the output. (parent_id)
-#' @return A data.frame with two columns, matching transcript IDs to gene IDs.
+#' @return A data.table with two columns, matching transcript IDs to gene IDs.
 #' 
 #' @export
 #'
 annot2ids <- function(annotfile, transc_header = "target_id", gene_header = "parent_id")
 {
-  annot <- read.delim(annotfile, comment.char = "#", header = FALSE)
-  t2g <- data.frame("transc_id" = gsub(".*transcript_id \"?(.+?)\"?;.*", "\\1", annot[[9]]),
+  annot <- as.data.table(read.delim(annotfile, comment.char = "#", header = FALSE))
+  annot <- annot[grepl("transcript_id", annot[[9]]) & grepl("gene_id", annot[[9]]), ]
+  t2g <- data.table("transc_id" = gsub(".*transcript_id \"?(.+?)\"?;.*", "\\1", annot[[9]]),
                     "gene_id" = gsub(".*gene_id \"?(.+?)\"?;.*", "\\1", annot[[9]]) )
+  # Clean up.
+  t2g <- na.omit(t2g)
   t2g <- unique(t2g)
   names(t2g) <- c(transc_header, gene_header)
   
