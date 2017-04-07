a71f33e554f6f0b5c7ba6938ae7fb96b0c780cd6
fruce-ki <jack_ohara_097@hotmail.com>
2017-04-07 11:50:42 +0100

    fix unjustified triggering of warning about qbootnum
    The trigger condition was conflicting with the “smart” default value of
    0. Despite 0 < 100, 0 is used as a flag to infer qbootnum automatically
    and should not trigger the warning over insufficient iterations.

--- R/func.R
+++ R/func.R
@@ -166,12 +166,12 @@ parameters_are_good <- function(slo, annot, name_A, name_B, varname, COUNTS_COL,
       if (minboots < 100) {
         warnmsg["toofewboots"] <- "Your quantifications have few bootstrap iterations, which reduces reproducibility of the calls."
       }
-      if (qbootnum < 100) {
+      if (qbootnum < 100 && qbootnum != 0) {
         warnmsg["toolowbootnum"] <- "The requested qbootnum is low, which reduces reproducibility of the calls."
       }
       bootcombos <- minboots^numsamples  # Conservative estimate.
       if (qbootnum >= bootcombos/100)
-        warnmsg["toomanyboots"] <- "The requested number of quantification bootstraps is very high, relative to the supplied data. Over 1% chance of duplicate iterations."
+        warnmsg["toomanyboots"] <- "The requested number of quantification bootstraps is very high, relatively to the supplied data. Over 1% chance of duplicate iterations."
       if (qbootnum >= maxmatrix/dim(annot)[1])
         return(list("error"=TRUE,"message"="The requested number of quantification bootstraps would exceed the maximum capacity of an R matrix."))
     } # else it is probably count data and qboot will be auto-set to FALSE
